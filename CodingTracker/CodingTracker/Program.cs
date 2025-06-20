7ca97d2dfbe167210461b7144120dc31caf7bf8d
MichaelKarcz <MichaelAKarcz@gmail.com>
2025-06-19 23:40:27 -0400

    Random changes learning Dapper

--- CodingTracker/CodingTracker/Program.cs
+++ CodingTracker/CodingTracker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Collections.Specialized;
+using System.Globalization;
 
 namespace CodingTracker
 {
@@ -7,10 +8,24 @@ namespace CodingTracker
     {
         public static void Main(string[] args)
         {
+            SQLHelper.CreateDatabaseIfNotExists();
 
             string sAttr = ConfigurationManager.AppSettings.Get("connectionString");
 
             Console.WriteLine($"The value of connectionString is {sAttr}");
+
+            var test = DateTime.Now.ToString("hh:mm tt");
+
+            DateTime testParse = DateTime.ParseExact("04:22 PM", "hh:mm tt", new CultureInfo("en-US"));
+
+            var timeOne = DateTime.ParseExact("05:30 PM", "hh:mm tt", new CultureInfo("en-US"));
+            var timeTwo = DateTime.ParseExact("08:30 AM", "hh:mm tt", new CultureInfo("en-US"));
+
+            var timeDifference = timeTwo - timeOne;
+
+            Console.WriteLine($"\nDateTime value: {test}");
+            Console.WriteLine($"\nDateTime parsed from string: {testParse}");
+            Console.WriteLine($"\nTime Difference between {timeTwo} and {timeOne} = {timeDifference}");
 
             Console.ReadKey();
 
--- CodingTracker/CodingTracker/SQLHelper.cs
+++ CodingTracker/CodingTracker/SQLHelper.cs
@@ -1,11 +1,53 @@
-﻿using Dapper;
+﻿using System.Configuration;
+using Dapper;
+using Microsoft.Data.Sqlite;
 
 namespace CodingTracker
 {
-    internal class SQLHelper
+    internal static class SQLHelper
     {
+        private static readonly string CONNECTION_STRING = ConfigurationManager.AppSettings.Get("connectionString");
+        private static readonly string TABLENAME = "codingSessions";
 
+        public static void CreateDatabaseIfNotExists()
+        {
+            try
+            {
+                var connection2 = new SqliteConnection(CONNECTION_STRING);
 
+                string commandText = $@"CREATE TABLE IF NOT EXISTS {TABLENAME} (
+                                              Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                              StartTime TEXT,
+                                              EndTime TEXT,
+                                              Duration TEXT
+                                              )";
+
+                connection2.Execute(commandText);
+
+                /*
+                using (var connection = new SqliteConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+                    var tableCmd = connection.CreateCommand();
+
+                    tableCmd.CommandText = $@"CREATE TABLE IF NOT EXISTS {TABLENAME} (
+                                              Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                              StartTime TEXT,
+                                              EndTime TEXT,
+                                              Duration TEXT
+                                              )";
+
+                    tableCmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+                */
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine($"\n\nThere was an error trying to instantiate the database. Error message: {e.Message}\n\n");
+            }
+        }
 
     }
 }
