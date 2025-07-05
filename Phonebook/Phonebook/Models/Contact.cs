16245c00ca1d726a066601fc25d454d644332378
MichaelKarcz <MichaelAKarcz@gmail.com>
2025-07-04 23:03:59 -0400

    Pushing to switch devices
    Just pushing so I can go work on my desktop instead of my laptop

---
+++ Phonebook/Phonebook/Models/Contact.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Phonebook.Models
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string Phone { get; set; }
+
+        public Contact()
+        {
+            Name = "";
+            Email = "";
+            Phone = "";
+        }
+
+        public Contact(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+    }
+
+    internal class ContactContext : DbContext
+    {
+        internal DbSet<Contact> Contacts { get; set; }
+        internal string DbPath { get; }
+
+        internal ContactContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "phonebook.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(ConfigurationManager.AppSettings.Get("phonebookConnectionString"));
+    }
+}
---
+++ Phonebook/Phonebook/Program.cs
@@ -0,0 +1,23 @@
+﻿using Phonebook.Models;
+
+namespace Phonebook;
+
+public class Program()
+{
+    public static void Main(string[] args)
+    {
+        using (ContactContext db = new ContactContext())
+        {
+            db.Contacts.Add(new Contact
+            {
+                Name = "Michael Karcz",
+                Email = "MichaelAKarcz@gmail.com",
+                Phone = "585-590-9534"
+            });
+
+            Console.WriteLine("Calling save changes...");
+            db.SaveChanges();
+            Console.WriteLine("Save changes complete");
+        }
+    }
+}
