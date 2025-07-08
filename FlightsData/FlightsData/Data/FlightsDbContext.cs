ce001e37d7cac1ff3a97216a2dda63d4b3552cf6
MichaelKarcz <MichaelAKarcz@gmail.com>
2025-07-08 14:32:35 -0400

    Complete Project
    Followed the tutorial to completion. Final working project for a "Flight data" web API.

---
+++ FlightsData/FlightsData/Data/FlightsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlightsData.Models;
+
+namespace FlightsData.Data
+{
+    public class FlightsDbContext : DbContext
+    {
+        public FlightsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
